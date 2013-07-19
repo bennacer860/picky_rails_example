2fa79f83f0a1b13a20b3cd3c724b9c5e68b405c2
bennacer <bennacer860@gmail.com>
2013-07-19 14:10:57 -0400

    add javascript search

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,17 @@
 class Book < ActiveRecord::Base
-  attr_accessible :author, :isbn, :subtitle, :title
+  	attr_accessible :author, :isbn, :subtitle, :title
+
+	after_commit :picky_index
+
+	# Index correctly, depending on whether it
+	# was destroyed or updated/created.
+	#
+	def picky_index
+	  if destroyed?
+	    BooksIndex.remove id
+	  else
+	    BooksIndex.replace self
+	  end
+	end
+
 end
