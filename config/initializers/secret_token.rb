9d0a3e8a3051312433dc96a709a39bd088ab5e7a
bennacer <bennacer860@gmail.com>
2013-07-19 12:05:25 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PickyRailsExample::Application.config.secret_token = '1b90466df161cb4c85c83e0188b93fea5e6414682c57c5b1dfce9601342f804b20dcafa1799ecb22e7673d472244b1fece61fb99c8cd0070ef35d151ba6baf1a'
